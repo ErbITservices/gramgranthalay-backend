80a71c605213f38972827186d786ebce8ec83527
ErbITservices <erbitservces@gmail.com>
2024-09-18 14:24:03 +0530

    first commit

--- sv.js
+++ sv.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cluster = require("cluster");
 const os = require("os")
-const app = express();
 const db = require("./db");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -19,47 +18,48 @@ const Motibhaiamin6rutes = require("./routes/Motibhaiamin6rutes");
 const Motibhaiamin7rutes = require("./routes/Motibhaiamin7rutes");
 const base_url = process.env.BASE;
 
-const corsOptions = {
-  origin: `${base_url}`,
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
-};
+
 const cpu = os.cpus().length;
+
+
 if (cluster.isPrimary) {
-  for (let index = 0; index < cpu.length; index++) {
+  for (let index = 0; index < cpu; index++) {
     cluster.fork();
-    
   }
+} else {
+ const app = express();
+
+
+  const corsOptions = {
+    origin: `${base_url}`,
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+  };
+
+  // console.log(cpu);
+
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json());
+  // app.use("/person",personRoutes);
+  // app.use('/menu',menuRoutes) ;
+  app.use("/form", formRouter);
+  app.use("/district", district);
+  app.use("/Count", Count);
+  app.use("/admin", adminRouter);
+  app.use("/Motibhaiamin1", Motibhaiamin1rutes);
+  app.use("/Motibhaiamin2", Motibhaiamin2rutes);
+  app.use("/Motibhaiamin3", Motibhaiamin3rutes);
+  app.use("/Motibhaiamin4", Motibhaiamin4rutes);
+  app.use("/Motibhaiamin5", Motibhaiamin5rutes);
+  app.use("/Motibhaiamin6", Motibhaiamin6rutes);
+  app.use("/Motibhaiamin7", Motibhaiamin7rutes);
+
+  const PORT = process.env.PORT || 5555;
+
+  db(
+    app.listen(PORT, () => {
+      console.log(`jaimin server is run on port ${PORT}`);
+    })
+  );
 }
-else {
-   app = express();
-  
-}
-// console.log(cpu);
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-// app.use("/person",personRoutes);
-// app.use('/menu',menuRoutes) ;
-app.use("/form", formRouter);
-app.use("/district", district);
-app.use("/Count", Count);
-app.use("/admin", adminRouter);
-app.use("/Motibhaiamin1", Motibhaiamin1rutes);
-app.use("/Motibhaiamin2", Motibhaiamin2rutes);
-app.use("/Motibhaiamin3", Motibhaiamin3rutes);
-app.use("/Motibhaiamin4", Motibhaiamin4rutes);
-app.use("/Motibhaiamin5", Motibhaiamin5rutes);
-app.use("/Motibhaiamin6", Motibhaiamin6rutes);
-app.use("/Motibhaiamin7", Motibhaiamin7rutes);
-
-const PORT = process.env.PORT || 5555;
-
-db(
-  app.listen(PORT, () => {
-    console.log(`jaimin server is run on port ${PORT}`);
-    console.log(base_url);
-  })
-);
-
-exports.app = app;
