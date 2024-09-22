ee841c7b05acb4213f5d1d8ddf42c3f5aa198252
ErbITservices <erbitservces@gmail.com>
2024-09-22 23:40:34 +0530

    first commit

--- sv.js
+++ sv.js
@@ -31,7 +31,7 @@ if (cluster.isPrimary) {
 
 
   const corsOptions = {
-    origin:  `${base_url}`|| `http://localhost:5173` ,
+    origin:  `${base_url}`|| `http://localhost:5173` ||,
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
   };
