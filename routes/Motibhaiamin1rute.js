f41e865b16748909547c9a8a60530ef40cf929e6
ErbITservices <erbitservces@gmail.com>
2024-11-06 22:51:42 +0530

    mihir

--- routes/Motibhaiamin1rute.js
+++ routes/Motibhaiamin1rute.js
@@ -20,6 +20,26 @@ Motibhaiamin1rutes.post("/", async (req, res) => {
 
 
 
+Motibhaiamin1rutes.get("/:lname", async (req, res) => {
+  try {
+    const lnametype = req.params.lname;
+    console.log(lnametype);
+
+    const data = await Motibhaiamin1.find({ lname: lnametype });
+    if (!data) {
+      res.status(500).json({
+        message: "No data found",
+      });
+    }
+    0;
+    console.log("data fatch");
+    res.status(200).json(data);
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 Motibhaiamin1rutes.get("/", async (req, res) => {
   try {
     const data = await Motibhaiamin1.find();
